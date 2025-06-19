3c8eb5e7ef1af2552d8ed29204adfeec35b38b5e
denys-omelchuk-planeks <denys.omelchuk@planeks.net>
2025-06-19 20:24:05 +0300

    removed redundant Model (Item) init migration

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
-from app.api.routes import items, login, private, users, utils, tasks
+from app.api.routes import login, private, users, utils, tasks
 from app.core.config import settings
 
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
-api_router.include_router(items.router)
 api_router.include_router(tasks.router)
 
 
