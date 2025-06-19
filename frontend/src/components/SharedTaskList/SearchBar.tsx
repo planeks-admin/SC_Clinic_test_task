d164af5003b94ee2765c197bd4f423f4ae003854
denys-omelchuk-planeks <denys.omelchuk@planeks.net>
2025-06-19 19:20:36 +0300

    implement SearchBar component

---
+++ frontend/src/components/SharedTaskList/SearchBar.tsx
@@ -0,0 +1,18 @@
+import type React from "react"
+
+interface Props {
+  title: string
+  onChange: (value: string) => void
+}
+
+const SearchBar: React.FC<Props> = ({ title, onChange }) => (
+  <input
+    type="text"
+    className="form-control mb-3"
+    placeholder="Search by title..."
+    value={title}
+    onChange={(e) => onChange(e.target.value)}
+  />
+)
+
+export default SearchBar
