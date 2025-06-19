bedba4d99ea1e136c4f7903e87c17c1d495a3c8d
denys-omelchuk-planeks <denys.omelchuk@planeks.net>
2025-06-19 19:19:41 +0300

    implement AddTask component

---
+++ frontend/src/components/SharedTaskList/AddTask.tsx
@@ -0,0 +1,168 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { type SubmitHandler, useForm } from "react-hook-form"
+import { useWebSocket } from "@/hooks/useWebSocket.ts"
+
+import {
+  Button,
+  DialogActionTrigger,
+  DialogTitle,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react"
+import { useState } from "react"
+import { FaPlus } from "react-icons/fa"
+
+import { type TaskCreate, TasksService } from "@/client"
+import type { ApiError } from "@/client/core/ApiError"
+import useCustomToast from "@/hooks/useCustomToast"
+import { handleError } from "@/utils"
+import {
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTrigger,
+} from "../ui/dialog"
+import { Field } from "../ui/field"
+
+const AddTask = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const queryClient = useQueryClient()
+  const { showSuccessToast } = useCustomToast()
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid, isSubmitting },
+  } = useForm<TaskCreate>({
+    mode: "onBlur",
+    criteriaMode: "all",
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  })
+
+  const { send } = useWebSocket()
+
+  const mutation = useMutation({
+    mutationFn: (data: TaskCreate) =>
+      TasksService.createTask({ requestBody: data }),
+    onSuccess: () => {
+      showSuccessToast("Task created successfully.")
+      reset()
+      setIsOpen(false)
+    },
+    onError: (err: ApiError) => {
+      handleError(err)
+    },
+    onSettled: () => {
+      send({ type: "event", data: "refresh_tasks" })
+      queryClient.invalidateQueries({ queryKey: ["tasks"] })
+    },
+  })
+
+  const onSubmit: SubmitHandler<TaskCreate> = (data) => {
+    mutation.mutate(data)
+  }
+
+  return (
+    <DialogRoot
+      size={{ base: "xs", md: "md" }}
+      placement="center"
+      open={isOpen}
+      onOpenChange={({ open }) => setIsOpen(open)}
+    >
+      <DialogTrigger asChild>
+        <Button value="add-item" my={4}>
+          <FaPlus fontSize="16px" />
+          Add Task
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DialogHeader>
+            <DialogTitle>Add Task</DialogTitle>
+          </DialogHeader>
+          <DialogBody>
+            <Text mb={4}>Fill in the details to add a new task.</Text>
+            <VStack gap={4}>
+              <Field
+                required
+                invalid={!!errors.title}
+                errorText={errors.title?.message}
+                label="Title"
+              >
+                <Input
+                  id="title"
+                  {...register("title", {
+                    required: "Title is required.",
+                  })}
+                  placeholder="Title"
+                  type="text"
+                />
+              </Field>
+
+              <Field
+                required
+                invalid={!!errors.description}
+                errorText={errors.description?.message}
+                label="Description"
+              >
+                <Input
+                  id="description"
+                  {...register("description", {
+                    required: "Description is required.",
+                  })}
+                  placeholder="Description"
+                  type="text"
+                />
+              </Field>
+              <Field
+                required
+                invalid={!!errors.assignee}
+                errorText={errors.assignee?.message}
+                label="Assignee"
+              >
+                <Input
+                  id="assignee"
+                  {...register("assignee", {
+                    required: "Assignee is required.",
+                  })}
+                  placeholder="Assignee"
+                  type="text"
+                />
+              </Field>
+            </VStack>
+          </DialogBody>
+
+          <DialogFooter gap={2}>
+            <DialogActionTrigger asChild>
+              <Button
+                variant="subtle"
+                colorPalette="gray"
+                disabled={isSubmitting}
+              >
+                Cancel
+              </Button>
+            </DialogActionTrigger>
+            <Button
+              variant="solid"
+              type="submit"
+              disabled={!isValid}
+              loading={isSubmitting}
+            >
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
+        <DialogCloseTrigger />
+      </DialogContent>
+    </DialogRoot>
+  )
+}
+
+export default AddTask
