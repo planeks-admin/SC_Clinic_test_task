982fa669aed068dcefb1b36703765631d88b5dd9
denys-omelchuk-planeks <denys.omelchuk@planeks.net>
2025-06-19 19:20:13 +0300

    implement ClaimTasks component

---
+++ frontend/src/components/SharedTaskList/ClaimTasks.tsx
@@ -0,0 +1,107 @@
+import {Button, DialogTitle, Text} from "@chakra-ui/react"
+import {useMutation, useQueryClient} from "@tanstack/react-query"
+import {useState} from "react"
+import {useForm} from "react-hook-form"
+import { useWebSocket } from "@/hooks/useWebSocket.ts"
+
+import {TasksService} from "@/client"
+import {
+    DialogActionTrigger,
+    DialogBody,
+    DialogCloseTrigger,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogRoot,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import useCustomToast from "@/hooks/useCustomToast"
+
+const ClaimTask = ({id}: { id: string }) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const queryClient = useQueryClient()
+    const {showSuccessToast, showErrorToast} = useCustomToast()
+    const {
+        handleSubmit,
+        formState: {isSubmitting},
+    } = useForm()
+
+    const { send } = useWebSocket()
+
+    const ClaimTask = async (id: string) => {
+        await TasksService.claimTask({id: id})
+    }
+
+    const mutation = useMutation({
+        mutationFn: ClaimTask,
+        onSuccess: () => {
+            showSuccessToast("The task was successfully claimed")
+            setIsOpen(false)
+        },
+        onError: () => {
+            showErrorToast("An error occurred while claimining task")
+        },
+        onSettled: () => {
+            send({ type: "event", data: "refresh_tasks" })
+            queryClient.invalidateQueries()
+        },
+    })
+
+    const onSubmit = async () => {
+        mutation.mutate(id)
+    }
+
+    return (
+        <DialogRoot
+            size={{base: "xs", md: "md"}}
+            placement="center"
+            role="alertdialog"
+            open={isOpen}
+            onOpenChange={({open}) => setIsOpen(open)}
+        >
+            <DialogTrigger asChild>
+                <Button variant="ghost" size="sm" colorPalette="red">
+                    Claim Task
+                </Button>
+            </DialogTrigger>
+
+            <DialogContent>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <DialogCloseTrigger/>
+                    <DialogHeader>
+                        <DialogTitle>Claim Task</DialogTitle>
+                    </DialogHeader>
+                    <DialogBody>
+                        <Text mb={4}>
+                            The status of this task will switch to "In Progress," and it will be removed from the Shared
+                            Task List.
+                            Are you sure you want to claim this task?
+                        </Text>
+                    </DialogBody>
+
+                    <DialogFooter gap={2}>
+                        <DialogActionTrigger asChild>
+                            <Button
+                                variant="subtle"
+                                colorPalette="gray"
+                                disabled={isSubmitting}
+                            >
+                                Cancel
+                            </Button>
+                        </DialogActionTrigger>
+                        <Button
+                            variant="solid"
+                            colorPalette="red"
+                            type="submit"
+                            loading={isSubmitting}
+                        >
+                            Claim Task
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </DialogRoot>
+    )
+}
+
+export default ClaimTask
