c0817a9d5dea6c32e8bdf85a9230e9cd139db8ae
denys-omelchuk-planeks <denys.omelchuk@planeks.net>
2025-06-19 19:21:11 +0300

    implement TaskActionMenu component

---
+++ frontend/src/components/Common/TaskActionMenu.tsx
@@ -0,0 +1,25 @@
+import { IconButton } from "@chakra-ui/react"
+import { BsThreeDotsVertical } from "react-icons/bs"
+import { MenuContent, MenuRoot, MenuTrigger } from "../ui/menu"
+
+import type { Task } from "@/client"
+import ClaimTask from "../SharedTaskList/ClaimTasks";
+
+interface TaskActionsMenuProps {
+  task: Task
+}
+
+export const TaskActionsMenu = ({ task }: TaskActionsMenuProps) => {
+  return (
+    <MenuRoot>
+      <MenuTrigger asChild>
+        <IconButton variant="ghost" color="inherit">
+          <BsThreeDotsVertical />
+        </IconButton>
+      </MenuTrigger>
+      <MenuContent>
+        <ClaimTask id={task.id} />
+      </MenuContent>
+    </MenuRoot>
+  )
+}
